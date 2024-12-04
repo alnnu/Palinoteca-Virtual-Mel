170e4d9926f05771653c6e2bec203e23bf16ab18
alnnu <luannf1020@gmail.com>
2024-12-04 18:11:25 -0300

    add: scenario model

--- app/models.py
+++ app/models.py
@@ -8,12 +8,23 @@ from user.models import User
 def upload_to(instance, filename):
     return '/'.join(['images', str(instance.user.id), filename])
 
+
+class Scenario(models.Model):
+    id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
+    criador_cenario = models.ForeignKey(User, on_delete=models.SET_NULL, null=True, default=None)
+    description = models.TextField()
+
+    objects = models.Manager()
+
 class Images(models.Model):
     id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
     image = models.ImageField(upload_to=upload_to)
 
     user = models.ForeignKey(User, on_delete=models.SET_NULL, null=True, blank=True, default=None)
 
+    scenario = models.ForeignKey(Scenario, on_delete=models.SET_NULL, null=True, blank=True, default=None)
+
     objects = models.Manager()
+
 
 
