d4ad26d40f296e701d5103936039f34c08a79523
alnnu <luannf1020@gmail.com>
2025-01-15 14:03:46 -0300

    add: pagination

--- api/settings.py
+++ api/settings.py
@@ -43,10 +43,11 @@ SIMPLE_JWT = {
 }
 
 REST_FRAMEWORK = {
-
     'DEFAULT_AUTHENTICATION_CLASSES': (
         'rest_framework_simplejwt.authentication.JWTAuthentication',
     ),
+    "DEFAULT_PAGINATION_CLASS": 'rest_framework.pagination.PageNumberPagination',
+    "PAGE_SIZE": 10,
 }
 ACCOUNT_USER_MODEL_USERNAME_FIELD = None
 ACCOUNT_USERNAME_REQUIRED = True
